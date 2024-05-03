ab12732c4739ad85bcae26c654109c98c6274e4f
Hal-ang <jpjgv05@gmail.com>
2024-05-03 22:31:49 +0900

    Implement Introduce crossTitle font layout

--- src/components/section/Introduce.tsx
+++ src/components/section/Introduce.tsx
@@ -1,43 +1,46 @@
 import { BonVivantFont } from "@/style/fonts";
 import Flex from "../Flex";
+import Glare from "../../../public/glare/glare.svg";
 import React from "react";
 import Text from "../Text";
 
 const CrossTitle = () => {
   return (
     <div
-      className={`${BonVivantFont.className} text-88pxr leading-71pxr mt-12pxr w-full`}
+      className={`${BonVivantFont.className} text-66pxr leading-58pxr medium:text-81pxr medium:leading-71pxr large:text-88pxr large:leading-77pxr mt-12pxr w-full`}
     >
-      <Text display="block" style={{ transform: "translate(-12px)" }}>
+      <Text display="block" style={{ transform: "translate(-0.75rem)" }}>
         NEVER
       </Text>
-      <Text
-        display="block"
-        className="text-right"
-        style={{ transform: "translate(12px)" }}
-      >
-        WITH
-      </Text>
-      <Text display="block" style={{ transform: "translate(-12px)" }}>
+      <Flex direction="row" align="start" justify="end">
+        <Glare className="flex-none inline w-44pxr h-44pxr medium:w-48pxr medium:h-48pxr large:w-52pxr large:h-52pxr" />
+        <Text
+          className="text-right"
+          style={{ transform: "translate(0.75rem)" }}
+        >
+          WITH
+        </Text>
+      </Flex>
+      <Text display="block" style={{ transform: "translate(-0.75rem)" }}>
         SADNESS
       </Text>
       <Text
         display="block"
         className="text-right"
-        style={{ transform: "translate(12px)" }}
+        style={{ transform: "translate(0.75rem)" }}
       >
         ALWAYS
       </Text>
-      <Text display="block" style={{ transform: "translate(-12px)" }}>
-        WITH
-      </Text>
-      <Text
-        display="block"
-        className="text-right"
-        style={{ transform: "translate(auto)" }}
-      >
-        GLADNESS
-      </Text>
+      <Flex direction="row" align="start" justify="start">
+        <Text display="block" style={{ transform: "translate(-0.75rem)" }}>
+          WITH
+        </Text>
+        <Glare className="flex-none  inline w-44pxr h-44pxr medium:w-48pxr medium:h-48pxr large:w-52pxr large:h-52pxr" />
+      </Flex>
+
+      <Flex direction="row" align="start" justify="end">
+        <Text style={{ transform: "translate(0.75rem)" }}>GLADNESS</Text>
+      </Flex>
     </div>
   );
 };
