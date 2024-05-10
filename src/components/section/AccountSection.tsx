4031d26d7aec747ffd06f90cbffebd31b457ed64
Hal-ang <jpjgv05@gmail.com>
2024-05-10 22:31:55 +0900

    Update account number mockup to real

--- src/components/section/AccountSection.tsx
+++ src/components/section/AccountSection.tsx
@@ -2,7 +2,6 @@
 
 import Account from "./Account";
 import Arcodion from "../Arcodion";
-import Flex from "../Flex";
 import React from "react";
 import Spacing from "../Spacing";
 import Text from "../Text";
@@ -14,38 +13,30 @@ const AccountSection = () => {
       <Title>{`GIFT FOR\nWEDDING\nCEREMONY`}</Title>
       <Spacing size={20} />
       <Arcodion>
-        <Arcodion.Header className="w-full py-21.5pxr border-t border-black">
+        <Arcodion.Header className="cursor-pointer w-full py-21.5pxr border-t border-black">
           <Text>신랑측 계좌번호</Text>
           <Arcodion.Arrow />
         </Arcodion.Header>
         <Arcodion.Content>
           <Account
             name="구태훈"
-            bankInfo={{ bankName: "신한은행", accountNumber: "110-123-45678" }}
+            bankInfo={{ bankName: "하나은행", accountNumber: "29891050568007" }}
           />
           <Spacing size={1} />
-          <Account
-            name="구태훈"
-            bankInfo={{ bankName: "신한은행", accountNumber: "110-123-45678" }}
-          />
         </Arcodion.Content>
       </Arcodion>
       <Spacing size={20} />
       <Arcodion>
-        <Arcodion.Header className="w-full py-21.5pxr border-t border-black">
+        <Arcodion.Header className="cursor-pointer w-full py-21.5pxr border-t border-black">
           <Text>신부측 계좌번호</Text>
           <Arcodion.Arrow />
         </Arcodion.Header>
         <Arcodion.Content>
           <Account
-            name="구태훈"
-            bankInfo={{ bankName: "신한은행", accountNumber: "110-123-45678" }}
+            name="김단희"
+            bankInfo={{ bankName: "우리은행", accountNumber: "1002560559559" }}
           />
           <Spacing size={1} />
-          <Account
-            name="구태훈"
-            bankInfo={{ bankName: "신한은행", accountNumber: "110-123-45678" }}
-          />
         </Arcodion.Content>
       </Arcodion>
     </section>
