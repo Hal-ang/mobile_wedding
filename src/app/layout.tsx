d8f48e4ddea9962b05cb65931613e85cbb0a40d0
Hal-ang <jpjgv05@gmail.com>
2024-05-11 23:05:00 +0900

    Update og image url

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export const metadata: Metadata = {
     description: "6월 8일 토요일 오후 4시 삼청각 일화당",
     images: [
       {
-        url: "https://github-production-user-asset-6210df.s3.amazonaws.com/68503014/329634204-e3ef77a2-eed3-4236-9dff-928b682b07dc.jpg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAVCODYLSA53PQK4ZA%2F20240510%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20240510T153246Z&X-Amz-Expires=300&X-Amz-Signature=92ed9f022eecd0669abff74b2f844c3843a2cb868edc13672832f1e6570ede84&X-Amz-SignedHeaders=host&actor_id=68503014&key_id=0&repo_id=795044003", // Must be an absolute URL
+        url: "https://cdn.jsdelivr.net/gh/Hal-ang/wedding_CDN_repo@master/sns.png", // Must be an absolute URL
         width: 720,
         height: 720
       }
