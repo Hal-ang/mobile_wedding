f1abbbf484858c4ef81cb09da99fd1d16c35764f
Hal-ang <jpjgv05@gmail.com>
2024-05-12 12:35:18 +0900

    Hotfix imageDetails min height on safari

--- src/components/ImageDetails.tsx
+++ src/components/ImageDetails.tsx
@@ -36,15 +36,16 @@ const ImageDetails = ({
   return (
     <Portal>
       <div
-        className="fixed min-h-screen w-full top-0 left-2/4 large:max-w-[430px]"
+        className="fixed w-full top-0 left-2/4 large:max-w-[430px]"
         style={{
+          minHeight: "100svh",
           backgroundColor: "rgba(0, 0, 0, 0.9)",
           transform: `translate(-50%)`
         }}
       ></div>
       <div
-        className="fixed w-full min-h-screen z-20 left-2/4  large:max-w-[430px]"
-        style={{ transform: `translate(-50%)` }}
+        className="fixed w-full z-20 left-2/4  large:max-w-[430px]"
+        style={{ transform: `translate(-50%)`, minHeight: "100svh" }}
       >
         <Flex
           className=" w-full py-4pxr px-10pxr"
